f0a1394d2ed646b9f16b7d0951085168a789e9df
Paul <69437294+Escbrk@users.noreply.github.com>
2025-09-13 17:08:23 -0600

    upd

--- script.js
+++ script.js
@@ -1,11 +1,29 @@
 const orderServiceBtn = document.querySelector(".hero-button");
-const closeModalBtn = document.querySelector(".close-modal-btn");
 const backdrop = document.querySelector(".backdrop");
+const body = document.body;
+const mobileMenu = document.querySelector(".mobile-menu");
+const burgerBtn = document.querySelector(".burger-btn");
+
+const closeModalBtn = document.querySelectorAll(".close-modal-btn");
+const closeOrderBtn = closeModalBtn[0];
+const closeMobileMenuBtn = closeModalBtn[1];
 
 orderServiceBtn.addEventListener("click", () => {
   backdrop.classList.add("is-open");
+  body.classList.add("is-modal-open");
 });
 
-closeModalBtn.addEventListener("click", () => {
+closeOrderBtn.addEventListener("click", () => {
   backdrop.classList.remove("is-open");
+  body.classList.remove("is-modal-open");
+});
+
+burgerBtn.addEventListener("click", () => {
+  mobileMenu.classList.add("is-mobile-menu-open");
+  body.classList.add("is-modal-open");
+});
+
+closeMobileMenuBtn.addEventListener("click", () => {
+  mobileMenu.classList.remove("is-mobile-menu-open");
+  body.classList.remove("is-modal-open");
 });
