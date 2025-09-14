61044b6a9fb6afd34310636ac2742fe7790ca39c
Paul <69437294+Escbrk@users.noreply.github.com>
2025-09-13 18:06:19 -0600

    upd

--- script.js
+++ script.js
@@ -5,25 +5,37 @@ const mobileMenu = document.querySelector(".mobile-menu");
 const burgerBtn = document.querySelector(".burger-btn");
 
 const closeModalBtn = document.querySelectorAll(".close-modal-btn");
-const closeOrderBtn = closeModalBtn[0];
-const closeMobileMenuBtn = closeModalBtn[1];
 
-orderServiceBtn.addEventListener("click", () => {
-  backdrop.classList.add("is-open");
-  body.classList.add("is-modal-open");
-});
+if (orderServiceBtn) {
+  orderServiceBtn.addEventListener("click", () => {
+    backdrop.classList.add("is-open");
+    body.classList.add("is-modal-open");
+  });
 
-closeOrderBtn.addEventListener("click", () => {
-  backdrop.classList.remove("is-open");
-  body.classList.remove("is-modal-open");
-});
+  closeModalBtn[0].addEventListener("click", () => {
+    backdrop.classList.remove("is-open");
+    body.classList.remove("is-modal-open");
+  });
+}
 
-burgerBtn.addEventListener("click", () => {
-  mobileMenu.classList.add("is-mobile-menu-open");
-  body.classList.add("is-modal-open");
-});
+if (closeModalBtn[1]) {
+  burgerBtn.addEventListener("click", () => {
+    mobileMenu.classList.add("is-mobile-menu-open");
+    body.classList.add("is-modal-open");
+  });
 
-closeMobileMenuBtn.addEventListener("click", () => {
-  mobileMenu.classList.remove("is-mobile-menu-open");
-  body.classList.remove("is-modal-open");
-});
+  closeModalBtn[1].addEventListener("click", () => {
+    mobileMenu.classList.remove("is-mobile-menu-open");
+    body.classList.remove("is-modal-open");
+  });
+} else {
+  burgerBtn.addEventListener("click", () => {
+    mobileMenu.classList.add("is-mobile-menu-open");
+    body.classList.add("is-modal-open");
+  });
+
+  closeModalBtn[0].addEventListener("click", () => {
+    mobileMenu.classList.remove("is-mobile-menu-open");
+    body.classList.remove("is-modal-open");
+  });
+}
